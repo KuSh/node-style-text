a089f2dcb22630480525bf39cb1dfc0cfba42b92
fisker <lionkay@gmail.com>
2025-10-14 17:36:29 +0800

    Remove useless `options`

--- browser.js
+++ browser.js
@@ -1,6 +1,5 @@
 import {factory} from './utilities.js'
 
-// eslint-disable-next-line unicorn/no-useless-undefined
-const styleText = factory((_formats, value) => value, undefined)
+const styleText = factory((_formats, value) => value)
 export default styleText
 export {styleText as stderr, styleText as stdout}
